69af1add71a4036ee5ded4d2723eaf6a59ae05c1
anthony <haythango123456789@gmail.com>
2021-02-28 11:09:56 -0400

    version de subida 1.2

--- src/crud/components/User.js
+++ src/crud/components/User.js
@@ -9,7 +9,7 @@ const User = ({user,borrar,editUser}) => {
             <div className="w-1/3 text-xs sm:text-lg">{apellido}</div>
             <div className="w-1/3 text-xs sm:text-lg">{ciudad}</div>
             <div className="w-1/5 text-xs sm:text-lg">{edad}</div> 
-            <button className="bg-green-400 h-5 sm:h-full px-2 text-white sm:text-2xl flex-grow text-xs " onClick={()=>editUser(user)}>&#128393;</button>  
+            <button className="bg-green-400 h-5 sm:h-full px-2 text-white sm:text-2xl flex-grow text-xs " onClick={()=>editUser(user)}>&#9842;</button>  
             <button className="bg-red-400 h-5 sm:h-full  text-white px-2 sm:text-2xl ml-1 flex-grow text-xs " onClick={()=>borrar(id)}>&#9747;</button> 
         </div>
     )
